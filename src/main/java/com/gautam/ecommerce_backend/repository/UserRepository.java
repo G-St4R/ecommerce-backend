be681ab0b585c44396ab072c67a16fa961a4be42
Gautam Sharma <gautamsharma@Gautams-MacBook-Air.local>
2026-04-02 12:20:44 +0530

    feat: add UserRepository

---
+++ src/main/java/com/gautam/ecommerce_backend/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.gautam.ecommerce_backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gautam.ecommerce_backend.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
